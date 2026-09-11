2ff3375093e3723ed761de7adb06975af750a88f
Hanne Jenifer R <hannejenifer9841@gmail.com>
2026-09-11 08:37:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with col5:
 
     color_scale = color_scales.get(selected_year, ["#00FFFF", "#2E8BC0", "#1E90FF"]) 
 
-    fig = px.density_map(
+    fig = px.density_mapbox(
         df_filtered,
         lat="Latitude",
         lon="Longitude",
